6247c109d8db30d33e85cb46fb46d61a39e56428
naveen108my <naveenmy108@gmail.com>
2021-11-07 18:58:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-4dfftw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4dfftw)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Naveen!</h1>
+      <p>React is an excellent library to work with!</p>
+    </div>
+  );
+}
